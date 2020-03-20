30da4f311aff0bbc01854ff3d2ca4df57cd6a8be
Lydia Benmamar <lydia.benmamar@gmail.com>
2020-03-20 08:46:33 +0100

    Update index + UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -24,9 +24,9 @@ class UserController extends MainController
     {
         if (!empty($_POST)) {
 
-            $user = ModelFactory::getModel('User')->readData($this->post->getPostVar('email'), 'email');
+            $user = ModelFactory::getModel('User')->readData($_POST['email'], 'email');
 
-            if (password_verify($this->post->getPostVar('pass'), $user['pass'])) {
+            if (password_verify($_POST['pass'], $user['pass'])) {
 
                 $this->session->createSession(
                     $user['id'],
@@ -35,12 +35,10 @@ class UserController extends MainController
                     $user['email']
                 );
 
-                $this->cookie->createAlert('Authentification réussie, bienvenue ' . $user['name'] .' !');
 
                 $this->redirect('home');
             }
-            $this->cookie->createAlert('Authentification échouée !');
-        }
+                 }
         return $this->twig->render('user/loginUser.twig');
     }
 
@@ -81,30 +79,29 @@ class UserController extends MainController
      */
     public function updateMethod()
     {
-        if (!empty($this->post->getPostArray())) {
+        if (!empty($_POST)) {
 
             if (!empty($this->files->getFileVar('name'))) {
                 $data['image'] = $this->files->uploadFile('img/user');
             }
 
-            $data['pass']   = password_hash($this->post->getPostVar('pass'), PASSWORD_DEFAULT);
-            $data['name']   = $this->post->getPostVar('name');
-            $data['email']  = $this->post->getPostVar('email');
+            $data['pass']   = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+            $data['name']   = $_POST['name'];
+            $data['email']  = $_POST['email'];
 
             ModelFactory::getModel('User')->updateData($this->get->getGetVar('id'), $data);
-            $this->cookie->createAlert('Modification réussie de l\'utilisateur sélectionné !');
+
 
             $this->redirect('home');
         }
         $user = ModelFactory::getModel('User')->readData($this->get->getGetVar('id'));
 
-        return $this->twig->render('user/updateUser.twig', ['user' => $user]);
+        return $this->render('user/updateUser.twig', ['user' => $user]);
     }
 
     public function deleteMethod()
     {
         ModelFactory::getModel('User')->deleteData($this->get->getGetVar('id'));
-        $this->cookie->createAlert('Utilisateur définitivement supprimé !');
 
         $this->redirect('home');
     }
